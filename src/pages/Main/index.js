78da38d672820ffb9230dac9db3adde59c920b27
Eric Batista <klose.eric31@gmail.com>
2020-02-15 23:03:48 -0300

    Header done

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/index';
+import Tabs from '../../components/Tabs/index';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+      <Tabs />
+    </Container>
+  );
+}
